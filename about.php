21b6ca8b4639f47588afa1bd4b947d4f258bbee8
PetejStone <petejstoney@gmail.com>
2019-03-03 15:06:53 -0800

    initial commit

---
+++ about.php
@@ -0,0 +1,21 @@
+<?php include('inc/header.php'); ?>
+<h2>About</h2>
+
+<main class="about">
+  <img src="images/janeausten.jpg" alt="Jane Austen">
+
+  <p>
+    Jane Austen was an English novelist known primarily for her six major novels, which
+    interpret, critique and comment upon the British landed gentry at the end of
+    the 18th century. Austen's plots often explore the dependence of women on
+    marriage in the pursuit of favourable social standing and economic security.
+    Her works critique the novels of sensibility of the second half of the 18th
+    century and are part of the transition to 19th-century literary realism.
+    Her use of biting irony, along with her realism, humour, and social commentary,
+    have long earned her acclaim among critics, scholars, and popular audiences
+    alike.
+  </p>
+</main>
+
+
+<?php include('inc/footer.php'); ?>
---
+++ index.php
@@ -0,0 +1,26 @@
+<?php include("inc/header.php"); ?>
+
+
+    <script src="js/main.js"></script>
+
+      <main class="home">
+        <h2>&#8220; The Person, be it gentleman or lady,
+        who has not the pleasure in a good novel, is
+        intolerably stupid &#8221;</h2>
+        <p>- Jane Austen</p>
+
+        <div class="intro">
+          <p>Austen is a world renowned English author,
+          whose works have been adapted numerous times into
+          movies, tv shows and plays.  Many who have stereotyped her work
+          as being "for women" have found her works to clever and witty
+          as they challenged the social norms of the day. Reading Austen
+          is not only entertaining, but it's beneficial in being culturally
+          literate, developing your theory of mind, and teaching important life
+          lessons.
+          </p>
+        </div>
+
+      </main>
+
+    <?php include("inc/footer.php") ?>
